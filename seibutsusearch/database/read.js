11ef825154729e8addd4797d44a180f1ff458c91
funa1wa2wa <95912233+funa1wa2wa@users.noreply.github.com>
2021-12-16 21:22:25 +0900

    ファイルの読み込みを可能にする

---
+++ seibutsusearch/database/read.js
@@ -0,0 +1,71 @@
+let database = [];
+
+const files = {
+  "biodic": [
+    "do03_chorui.csv",
+    "do04_honyurui.csv",
+    "do04_kairui.csv",
+    "do04_konchurui_cho.csv",
+    "do04_konchurui_ga.csv",
+    "do04_konchurui_kochu.csv",
+    "do04_konchurui_semi.csv",
+    "do04_konchurui_tonbo.csv",
+    "do04_ryoseiruihachurui.csv",
+    "do04_tansuigyorui.csv",
+    "do05_honyurui.csv",
+    "do05_kairui.csv",
+    "do05_konchurui_cho.csv",
+    "do05_konchurui_ga.csv",
+    "do05_konchurui_kochu.csv",
+    "do05_konchurui_semi.csv",
+    "do05_konchurui_tonbo.csv",
+    "do05_ryoseiruihachurui.csv",
+    "do05_tansuigyorui.csv"
+  ],
+  "biodic02":[
+    "do02_chorui_1.csv",
+    "do02_chorui_2.csv",
+    "do02_honyurui.csv",
+    "do02_konchurui.csv",
+  ],
+  "biodic06":[
+    "do06_honyurui.csv"
+  ]
+}
+for(let type of Object.keys(files)){
+  for(let fileName of files[type]){
+    fetch(`biodic/${fileName}`)
+    .then(res=>res.text())
+    .then(text=>{
+      let obj = CSVtoAry(text);
+      switch(type){
+        case "biodic":
+          database = database.concat(obj.map(x=>({
+            name : x[4],
+            meshcode : x[5],
+            date : x[6]
+          })));
+          break;
+        case "biodic":
+          database = database.concat(obj.map(x=>({
+            name : x[4],
+            meshcode : x[0],
+            date : "78--"
+          })));
+          break;
+        case "biodic":
+          database = database.concat(obj.map(x=>({
+            name : x[0],
+            meshcode : x[1].slice(0, 6),
+            date : "04--",
+            year : 2004
+          })));
+          break;
+      }
+    });
+  }
+}
+
+function CSVtoAry(str){
+  return str.replaceAll("\"", "").split("\n").map(x=>x.split(",")).filter(x=>x.length>1);
+}

f99782b535f7237c8d2c59369e37939816b93281
funa1wa2wa <95912233+funa1wa2wa@users.noreply.github.com>
2021-12-26 17:09:05 +0900

    Update read.js

--- seibutsusearch/database/read.js
+++ seibutsusearch/database/read.js
@@ -1,46 +1,54 @@
 let database = [];
+let gairai = {};
+let type = {};
 
 const files = {
   "biodic": [
-    "do03_chorui.csv",
-    "do04_honyurui.csv",
-    "do04_kairui.csv",
-    "do04_konchurui_cho.csv",
-    "do04_konchurui_ga.csv",
-    "do04_konchurui_kochu.csv",
-    "do04_konchurui_semi.csv",
-    "do04_konchurui_tonbo.csv",
-    "do04_ryoseiruihachurui.csv",
-    "do04_tansuigyorui.csv",
-    "do05_honyurui.csv",
-    "do05_kairui.csv",
-    "do05_konchurui_cho.csv",
-    "do05_konchurui_ga.csv",
-    "do05_konchurui_kochu.csv",
-    "do05_konchurui_semi.csv",
-    "do05_konchurui_tonbo.csv",
-    "do05_ryoseiruihachurui.csv",
-    "do05_tansuigyorui.csv"
+    "biodic/03_chorui.csv",
+    "biodic/04_honyurui.csv",
+    "biodic/04_kairui.csv",
+    "biodic/04_konchurui_cho.csv",
+    "biodic/04_konchurui_ga.csv",
+    "biodic/04_konchurui_kochu.csv",
+    "biodic/04_konchurui_semi.csv",
+    "biodic/04_konchurui_tonbo.csv",
+    "biodic/04_ryoseiruihachurui.csv",
+    "biodic/04_tansuigyorui.csv",
+    "biodic/05_honyurui.csv",
+    "biodic/05_kairui.csv",
+    "biodic/05_konchurui_cho.csv",
+    "biodic/05_konchurui_ga.csv",
+    "biodic/05_konchurui_kochu.csv",
+    "biodic/05_konchurui_semi.csv",
+    "biodic/05_konchurui_tonbo.csv",
+    "biodic/05_ryoseiruihachurui.csv",
+    "biodic/05_tansuigyorui.csv"
   ],
   "biodic02":[
-    "do02_chorui_1.csv",
-    "do02_chorui_2.csv",
-    "do02_honyurui.csv",
-    "do02_konchurui.csv",
+    "biodic/02_chorui_1.csv",
+    "biodic/02_chorui_2.csv",
+    "biodic/02_honyurui.csv",
+    "biodic/02_konchurui.csv",
   ],
   "biodic06":[
-    "do06_honyurui.csv"
+    "biodic/06_honyurui.csv"
+  ],
+  "gairai":[
+    "gairai.csv"
+  ],
+  "type": [
+    "type.csv"
   ]
 }
 for(let type of Object.keys(files)){
   for(let fileName of files[type]){
-    fetch(`https://funa1wa2wa.github.io/seibutsusearch/database/biodic/${fileName}`)
+    fetch(`https://funa1wa2wa.github.io/seibutsusearch/database/${fileName}`)
     .then(res=>res.text())
     .then(text=>{
-      let obj = CSVtoAry(text);
+      let ary = CSVtoAry(text);
       switch(type){
         case "biodic":
-          database = database.concat(obj.map(x=>({
+          database.push(...ary.map(x=>({
             name : x[4],
             meshcode : x[5],
             date : x[6],
@@ -48,7 +56,7 @@ for(let type of Object.keys(files)){
           })));
           break;
         case "biodic02":
-          database = database.concat(obj.map(x=>({
+          database.push(...ary.map(x=>({
             name : x[4],
             meshcode : x[0],
             date : "78--",
@@ -56,18 +64,34 @@ for(let type of Object.keys(files)){
           })));
           break;
         case "biodic06":
-          database = database.concat(obj.map(x=>({
+          database.push(...ary.map(x=>({
             name : x[0],
             meshcode : x[1].slice(0, 6),
             date : "04--",
             year : 2004
           })));
           break;
+        case "gairai":
+          ary.map(x=>{
+            gairai[x[4]] = {
+              origin: x[0],
+              specify: x[2],
+              remark: x[10]=="*"?"":x[10],
+              distribution: x[11]
+            }
+          });
+          break;
+        case "type":
+          ary.map(x=>{
+            type[x[0]] = x[1]
+          });
+          break;
       }
     });
   }
 }
 
+// CSV形式のString -> 2次元配列
 function CSVtoAry(str){
   return str.replaceAll("\"", "").split(/\r?\n/).map(x=>x.split(",")).filter(x=>x.length>1);
 }
